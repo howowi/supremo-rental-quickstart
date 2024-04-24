de6c0ca2cbd6311aad40d129bf8f178cf8d6958c
howie.owi@oracle.com <owiweihow@gmail.com>
2024-04-24 11:07:49 +0800

    updated readme and included images

--- terraform/devops.tf
+++ terraform/devops.tf
@@ -1,7 +1,7 @@
 ## ----- Project ----- ##
 resource "oci_ons_notification_topic" "devops-notification-topic" {
     compartment_id = var.compartment_ocid
-    name = "devops-notification-topic"
+    name = "devops-notification-topic-${random_id.tag.hex}"
 }
 
 resource "oci_devops_project" "supremo-devops-project" {
